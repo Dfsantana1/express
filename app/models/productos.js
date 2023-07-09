7169babd14da67a9aa2696a8ee97729c1fbb3b08
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-07-09 12:19:10 -0500

    email

--- app/models/productos.js
+++ app/models/productos.js
@@ -1,4 +1,14 @@
 const db = require('../../config/database');
+const nodemailer = require('nodemailer');
+
+// Configurar el transporte de nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '',
+    pass: ''
+  }
+});
 
 class Product {
   static async crearProducto(nombre, precio, categoriaId) {
@@ -23,6 +33,37 @@ class Product {
 
       const [rows] = await db.query(query, params);
       return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async verificarStock(productoId) {
+    try {
+      const query = 'SELECT stock FROM productos WHERE id = ?';
+      const [result] = await db.query(query, [productoId]);
+      const stock = result[0].stock;
+      
+      if (stock <= 0) {
+        console.log(`¡Alerta de stock! Producto con ID ${productoId} sin stock.`);
+
+        // Configurar el contenido del correo
+        const mailOptions = {
+          from: 'dsantanafernandez@gmail.com',
+          to: 'd.santana@utp.edu.co',
+          subject: 'Alerta de stock',
+          text: `¡Alerta de stock! El producto con ID ${productoId} está sin stock.`
+        };
+
+        // Enviar el correo electrónico
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error('Error al enviar el correo:', error);
+          } else {
+            console.log('Correo enviado:', info.response);
+          }
+        });
+      }
     } catch (error) {
       throw error;
     }
