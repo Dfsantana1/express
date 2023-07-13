618b6690f6f50a115bfccbd8ad3c09ea1ff4521e
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-07-12 21:59:08 -0500

    cart

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const app = express();
 
 const port = 5000;
 
-const productos = require('./app/routes/productRoutes');
-  const carrito = require('./app/routes/carrito');
+const products = require('./app/routes/productRoutes');
+  const cart = require('./app/routes/carrito');
 const registroRouter = require('./app/routes/registro');
 const loginRouter = require('./app/routes/login'); // Agregado: Ruta para el login
 
 app.use(cors()); // Habilitar CORS para todas las rutas
 app.use(express.json()); // Parsear el cuerpo de las solicitudes como JSON
 
-app.use(carrito);
+app.use(cart);
 app.use(registroRouter);
 app.use(loginRouter);
-app.use(productos);
+app.use(products);
 
 app.listen(port, () => {
   console.log('La aplicación está en línea');
