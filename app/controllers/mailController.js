ea66b849a368df72612deabf741cae36c25efe0f
Kirby-07 <l.gonzalez1@utp.edu.co>
2023-07-14 02:31:51 -0500

    Controller y  Model
    Controlle

---
+++ app/controllers/mailController.js
@@ -0,0 +1,26 @@
+const transporter = require("../models/sendMailModel");
+
+const sendMailUser = async (req, res) => {
+  try {
+    const user = await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: "l.gonzalez1@utp.edu.co",
+      subject: "Compra realizada User",
+      text: "Hola, esta fue tu compra",
+    });
+    console.log({ user });
+    const admin = await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: "l.gonzalez1@utp.edu.co",
+      subject: "Compra realizada Admin",
+      text: "Hola, esta fue tu compra realizada por luis",
+    });
+    console.log({ admin });
+    res.status(200).json({ message: "Correo enviado exitosamente" });
+  } catch (error) {
+    console.error("Error al enviar el correo:", error);
+    res.status(500).json({ message: "Error al enviar el correo" });
+  }
+};
+
+module.exports = sendMailUser;
