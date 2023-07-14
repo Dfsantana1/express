db2ba60465e1dec4d7cafa2ac5dc9ac6b9dd05df
Kirby-07 <l.gonzalez1@utp.edu.co>
2023-07-14 03:24:07 -0500

    Mensaje Correo

--- app/controllers/mailController.js
+++ app/controllers/mailController.js
@@ -6,17 +6,17 @@ console.log(email);
 
   try {
     const user = await transporter.sendMail({
-      from: process.env.EMAIL,
+      from: '"FlexStride" <Felipegb24510@gmail.com>',
       to: email,
-      subject: "Compra realizada User",
-      text: "Hola, esta fue tu compra",
+      subject: "Hemos recibido correctamente tu pago ;)",
+      text: "Hola, has realizado una compra en FlexStride, nos alegra que confies en nosotros. Consulta nuestra página para consultar el estado de tu envío",
     });
     console.log({ user });
     const admin = await transporter.sendMail({
-      from: process.env.EMAIL,
-      to: "l.gonzalez1@utp.edu.co",
-      subject: "Compra realizada Admin",
-      text: "Hola, esta fue tu compra realizada por luis",
+      from: '"FlexStride" <Felipegb24510@gmail.com>',
+      to: "adm.tiendita.react@gmail.com",
+      subject: "Compra realizada ",
+      text: "Hola, han realizado una compra en tu Ecommerce, por favor verifica en tu Dashboard para más información",
     });
     console.log({ admin });
     res.status(200).json({ message: "Correo enviado exitosamente" });
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const port = 5000;
+const port = 3000;
 
 const products = require('./app/routes/productRoutes');
   const cart = require('./app/routes/carrito');
