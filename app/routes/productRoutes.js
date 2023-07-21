89210ae287cb8826704e217f634c59a19c40b2f3
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-07-20 19:20:59 -0500

    goo

--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -5,7 +5,7 @@ const sendMailUser = require('../controllers/mailController');
 const router = express.Router();
 
 // Definir la ruta GET '/products/:productId?' y llamar a productController.obtenerProductos para manejar la solicitud
-router.get('/products/:productId?', productController.obtenerProductos);
+router.get('/api/products/:productId?', productController.obtenerProductos);
 
 // Definir la ruta POST '/products' y llamar a productController.agregarProducto para manejar la solicitud
 router.put('/products', productController.editarProducto);
